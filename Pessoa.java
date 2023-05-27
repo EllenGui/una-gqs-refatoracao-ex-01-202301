6c0324169a3b181d400ee7b2527dc3ad231c865c
Ellen Guimarães <ellenguimaraes1000@gmail.com>
2023-05-27 14:53:54 -0300

    refactor: adicionando private na classe Pessoa

--- Pessoa.java
+++ Pessoa.java
@@ -1,14 +1,14 @@
 import java.util.Scanner;
 
 public class Pessoa {
-  public String nome;
-  public int idade;
-  public String cartaoVacinaEmDia;
-  public String teveSintomasRecentemente;
-  public String teveContatoComPessoasSintomaticas;
-  public String estaRetornandoViagem;
-  public int porcentagemInfeccao;
-  public String orientacaoFinal;
+  private String nome;
+  private int idade;
+  private String cartaoVacinaEmDia;
+  private String teveSintomasRecentemente;
+  private String teveContatoComPessoasSintomaticas;
+  private String estaRetornandoViagem;
+  private int porcentagemInfeccao;
+  private String orientacaoFinal;
 
   
   
@@ -21,5 +21,54 @@ public class Pessoa {
   public void coletaIdade(Pessoa pessoa, Scanner scanner) {
     System.out.println("\nInforme a sua idade: ");
     idade = scanner.nextInt();
+}
+
+  public String getNome(){
+    return nome;
+}
+  public void setNome(String nome){
+    this.nome = nome;
+}
+  public int getIdade(){
+    return idade;
+  }
+  public void setIdade(int idade){
+    this.idade = idade;
+  }
+  public String getcartaoDeVacinaEmDia(){
+    return cartaoVacinaEmDia;
+  }
+  public void setcartaoDeVacinaEmDia(String cartaoDeVacinaEmDia){
+    this.cartaoVacinaEmDia = cartaoDeVacinaEmDia;
+  }
+  public String getteveSintomasRecentemente(){
+    return teveSintomasRecentemente;
+  }
+  public void setteveSintomasRecentemente(String teveSintomasRecentemente){
+    this.teveSintomasRecentemente = teveSintomasRecentemente;
+  }
+  public String getteveContatoComPessoasSintomaticas(){
+    return teveContatoComPessoasSintomaticas;
+  }
+  public void setteveContatoComPessoasSintomaticas(String teveContatoComPessoasSintomaticas){
+    this.teveContatoComPessoasSintomaticas = teveContatoComPessoasSintomaticas;
+  }
+  public String getestaRetornandoViagem(){
+    return estaRetornandoViagem;
+  }
+  public void setestaRetornandoViagem(String estaRetornandoViagem){
+    this.estaRetornandoViagem = estaRetornandoViagem;
+  }
+  public int getporcentagemInfeccao(){
+    return porcentagemInfeccao;
+  }
+  public void setporcentagemInfeccao(int porcentagemInfeccao){
+    this.porcentagemInfeccao = porcentagemInfeccao;
+  }
+  public String getorientacaoFinal(){
+    return orientacaoFinal;
+  }
+  public void setorientacaoFinal(String orientacaoFinal){
+    this.orientacaoFinal = orientacaoFinal;
   }
 }
